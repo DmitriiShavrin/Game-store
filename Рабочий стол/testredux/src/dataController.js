a9679f604ea34d989235d4d66422a7d42a3605b6
DmitriiShavrin <102305084+DmitriiShavrin@users.noreply.github.com>
2022-04-09 11:06:49 +0600

    Delete Рабочий стол/testredux directory

--- Рабочий стол/testredux/src/dataController.js
+++
@@ -1,7 +0,0 @@
-export default () => {
-    return async (dispatch) => {
-        let res = await fetch('/data.json');
-        let data = await res.json();
-        dispatch({ type: 'SET_DATA', payload: data });
-    }
-}
